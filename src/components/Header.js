21aee77fb093547485027ab5ec3446ed04114db2
yashas8197 <yashasjr7@gmail.com>
2024-05-23 21:55:45 +0530

    feat: signout user

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,35 @@
 import React from "react";
-
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
   return (
-    <div>
-      <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-40 ">
-        <img
-          className="w-44  "
-          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-          alt="netflix logo"
-        />
-      </div>
+    <div className="w-screen absolute px-4 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img
+        className="w-44  "
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="netflix logo"
+      />
+      {user && (
+        <div className="flex p-2">
+          <img src={user.photoURL} className="w-12 h-12" />
+          <button onClick={handleSignOut} className="font-bold text-white">
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,12 +4,18 @@ import { checkValidData } from "../utils/validate";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const email = useRef(null);
   const password = useRef(null);
@@ -38,6 +44,25 @@ const Login = () => {
       )
         .then((userCredential) => {
           const user = userCredential.user;
+          updateProfile(user, {
+            displayName: "Yashas",
+            photoURL: "https://avatars.githubusercontent.com/u/130334248?v=4",
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -52,11 +77,11 @@ const Login = () => {
       )
         .then((userCredential) => {
           const user = userCredential.user;
+          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          console.log(errorMessage);
           setErrorMessage(`Error ${errorCode}: ${errorMessage}`);
         });
     }
